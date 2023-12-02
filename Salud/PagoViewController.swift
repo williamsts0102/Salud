5086c3adfc1e09fe12ab8daa8d47ec41393a095c
LEOPEIXOTO542 <116983534+LEOPEIXOTO542@users.noreply.github.com>
2023-12-02 14:12:54 -0500

    vista pagos

---
+++ Salud/PagoViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PagoViewController.swift
+//  Salud
+//
+//  Created by DAMII on 2/12/23.
+//
+
+import UIKit
+
+class PagoViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var numeroTarjetaTextField: UITextField!
+    
+    
+    @IBOutlet weak var anoTextField: UITextField!
+    
+    
+    @IBOutlet weak var cvvTextField: UITextField!
+    
+    
+    @IBOutlet weak var numeroDocumentoTextField: UITextField!
+    
+    
+    @IBOutlet weak var razonSocialTextField: UITextField!
+    
+    
+    @IBOutlet weak var telefonoTextField: UITextField!
+    
+    
+    
+    
+    @IBOutlet weak var pagarButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    @IBAction func pagarButtonAction(_ sender: UIButton) {
+    }
+    
+
+}
